1d971924f72594e5fd0dcb7c7cd1b9de83b51b0a
Julian David <julianfullstackdev@gmail.com>
2023-07-28 21:38:04 -0500

    add favicon, add responsive

--- app/page.tsx
+++ app/page.tsx
@@ -6,40 +6,44 @@ import { MdPlayCircle } from 'react-icons/md'
 export default async function Login() {
   return (
     <div className='min-h-screen bg-[#FFA53B]'>
-      <div className='mx-auto flex max-w-6xl pt-14'>
-        <div className='relative w-2/3 px-20'>
+      <div className='mx-auto flex flex-col pt-8 sm:max-w-6xl sm:flex-row sm:pt-14'>
+        <div className='relative w-full px-6 sm:w-2/3 sm:px-20'>
           <h3 className='flex items-center gap-2  text-4xl font-bold text-black/90'>
-            <PiPlayFill className='text-3xl text-black' />
+            <PiPlayFill className='text-2xl text-black sm:text-3xl' />
             favor
           </h3>
 
-          <h1 className='mt-32 text-7xl font-semibold'>Get Favor</h1>
-          <p className='mt-10 text-3xl font-medium text-black/80'>
+          <h1 className='mt-16 text-5xl font-semibold sm:mt-32 sm:text-7xl'>
+            Get Favor
+          </h1>
+          <p className='mt-4 text-2xl font-medium text-black/80 sm:mt-10 sm:text-3xl'>
             The dating app that works in <br />
             <span>your favor</span>
           </p>
 
-          <h4 className='mb-7 mt-20 text-xl font-medium'>Stay tuned:</h4>
+          <h4 className='mb-5 mt-10 text-xl font-medium sm:mb-7 sm:mt-20'>
+            Stay tuned:
+          </h4>
           <form>
-            <div className='space-y-4'>
+            <div className='space-y-3 sm:space-y-4'>
               <input
                 type='email'
                 placeholder='Your email*'
-                className='w-full rounded-full px-6 py-3 shadow-sm shadow-black/50 outline-[#FFA53B] placeholder:text-black/70'
+                className='w-full rounded-full px-6 py-2.5 shadow-sm shadow-black/50 outline-[#FFA53B] placeholder:text-black/70 sm:py-3'
               />
               <input
                 type='text'
                 placeholder='Your city*'
-                className='w-full rounded-full px-6 py-3 shadow-sm shadow-black/50 placeholder:text-black/70'
+                className='w-full rounded-full px-6 py-2.5 shadow-sm shadow-black/50 placeholder:text-black/70 sm:py-3'
               />
             </div>
 
-            <button className='hover mt-7 rounded-full bg-black/70 px-10 py-3 font-medium text-white transition-colors hover:bg-black/90'>
+            <button className='hover mt-5 rounded-full bg-black/70 px-10 py-2.5 font-medium text-white transition-colors hover:bg-black/90 sm:mt-7 sm:py-3'>
               Send
             </button>
           </form>
 
-          <h3 className='mt-20 text-center text-xl font-medium text-black/80'>
+          <h3 className='mt-10 text-center text-xl font-medium text-black/80'>
             Available soon on:
           </h3>
           <div className='relative flex justify-center'>
@@ -52,8 +56,8 @@ export default async function Login() {
             />
           </div>
         </div>
-        <div className='flex w-1/3 flex-col items-center'>
-          <h2 className='pt-10 text-center text-2xl font-bold'>
+        <div className='mt-8 flex h-[28rem] w-full flex-col items-center sm:mt-0 sm:h-auto sm:w-1/3'>
+          <h2 className='text-center text-xl font-bold sm:text-2xl'>
             Favor in 2 minutes:
           </h2>
           <a
@@ -66,10 +70,10 @@ export default async function Login() {
               src='/demo-preview.png'
               fill
               alt='Login Background'
-              className='object-cover opacity-90 transition-opacity group-hover:opacity-100'
+              className='object-contain opacity-90 transition-opacity group-hover:opacity-100'
             />
 
-            <MdPlayCircle className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-7xl text-white/80 transition-colors group-hover:text-white' />
+            <MdPlayCircle className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-5xl text-white/80 transition-colors group-hover:text-white sm:text-7xl' />
           </a>
         </div>
       </div>
