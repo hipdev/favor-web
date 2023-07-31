8bf372402494a5152f5fecd4336c42f9e7931c17
Julian David <julianfullstackdev@gmail.com>
2023-07-31 12:00:03 -0500

    add sanity client

---
+++ lib/sanity-client.ts
@@ -0,0 +1,27 @@
+import { createClient, type SanityClient } from 'next-sanity'
+
+export function getClient({
+  preview,
+}: {
+  preview?: { token: string }
+}): SanityClient {
+  const client = createClient({
+    projectId: '0sumf3xi',
+    dataset: 'production',
+    apiVersion: '2023-06-20',
+    useCdn: true,
+    perspective: 'published',
+  })
+  if (preview) {
+    if (!preview.token) {
+      throw new Error('You must provide a token to preview drafts')
+    }
+    return client.withConfig({
+      token: preview.token,
+      useCdn: false,
+      ignoreBrowserTokenWarning: true,
+      perspective: 'previewDrafts',
+    })
+  }
+  return client
+}
