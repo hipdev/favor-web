2a72cb679a3498dea4c4b3e0d8a7551d1988c017
Julian David <julianfullstackdev@gmail.com>
2023-07-31 22:49:36 -0500

    test redirect

--- components/auth/login-google.tsx
+++ components/auth/login-google.tsx
@@ -8,7 +8,7 @@ export default function LoginGoogle() {
   const loginGoogle = async () => {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
-      options: { redirectTo: `https://www.getfavor.app/` },
+      options: { redirectTo: `${location.origin}/admin/users` },
     })
   }
 
